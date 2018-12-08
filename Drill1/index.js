945c7a28a883f70c749bb4c2e7b68bfb7f40139d
Meghana <joshimeg30@gmail.com>
2018-12-08 00:17:58 -0500

    Update index.js

--- Drill1/index.js
+++ Drill1/index.js
@@ -1,21 +1,17 @@
 'use strict';
 function handleImgClick(){
   $('.thumbnail').on ('click', function(e){
-
+  //find and store the src attribute of thumbnail image clicked
     let imgSrc = $(this).find('img').attr("src");
-
-
+  //find and store the alt attribute of thumbnail image clicked
     let imgAlt = $(this).find('img').attr("alt");
+    // change the  image source of main hero div image to the one clicked
+    
+    console.log(imgSrc, imgAlt);
     $('.hero').find('img').attr("src",imgSrc);
+    //// change the  alt source of main hero div image to the one clicked
     $('.hero').find('img').attr("alt",imgAlt);
 
-
-    console.log(imgSrc, imgAlt)
-    // change the source of main hero div image to the one clicked
-
-
   });
-
-
 }
 $(handleImgClick);
